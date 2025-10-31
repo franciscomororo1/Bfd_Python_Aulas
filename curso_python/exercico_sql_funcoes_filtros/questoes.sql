df27d049acf5bd9fc3f90b1c571d0f8b2dd0b5aa
franciscomororo1 <junior.mororo@gmail.com>
2025-10-31 18:41:13 +0000

    Add Exercício de SQL funções e filtros

---
+++ curso_python/exercico_sql_funcoes_filtros/questoes.sql
@@ -0,0 +1,19 @@
+Questão 1
+
+SELECT COUNT(*) AS total_alunos FROM Aluno;
+
+Questão 2
+
+SELECT MIN(mensalidade) AS menor_mensalidade FROM Curso;
+
+Questão 3
+
+SELECT MAX(nota1) AS maior_nota1 FROM Aluno;
+
+Questão 4
+
+SELECT SUM(mensalidade) AS total_mensalidades FROM Curso;
+
+Questão 5
+
+SELECT AVG(nota2) AS media_geral_nota2 FROM Aluno;
