4a39e57e02b1df7f5440b2087009270a510fd17a
franciscomororo1 <junior.mororo@gmail.com>
2025-11-06 23:23:18 +0000

    Conectando o banco MONGODB

--- curso_python/aulas/mongo.py
+++ curso_python/aulas/mongo.py
@@ -1,4 +1,28 @@
 import pymongo
-import dotenv
-cliente = pymongo.MongoClient
-print(cliente)
+import os
+from dotenv import load_dotenv
+
+
+#Carregar variaveis do arquivo .env
+load_dotenv()
+
+#Acessa a variavel de ambiente
+MONGO_URI = os.getenv("MONGO_URI")
+
+#Verfica se a URI foi carregada
+
+if MONGO_URI is None:
+    print("não encontrado")
+else:
+    try:
+        conn = pymongo.MongoClient(MONGO_URI)
+
+        conn.admin.command('ping')
+        print("conectado com sucesso!")
+        print(conn)
+        conn.close()
+
+    except pymongo.errors.ConnectionFailure as e:
+        print(f"Falha ao conectar ao MONGODB {e}")
+
+        
